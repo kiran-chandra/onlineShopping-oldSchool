0662daa7589bf87a95647ac87051e98ba6654930
Chandra Kiran <ankir.nck@gmail.com>
2021-01-26 10:52:41 +0530

    Added static resources

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # onlineShopping-oldSchool
+
+Practicing code from:  https://www.youtube.com/channel/UC6U4a69UEZnWofbq7bMs7Ww
---
+++ onlineshopping/src/main/java/com/oldways/onlineshopping/controller/PageController.java
@@ -0,0 +1,37 @@
+package com.oldways.onlineshopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+	
+	@RequestMapping(value = {"/", "/home", "/index"})
+	public ModelAndView index() {
+		ModelAndView mav = new ModelAndView("page");
+		mav.addObject("greeting", "Welcome to draft pages of OnlineShopping");
+		return mav;
+	}
+	
+//	@RequestMapping(value="/test")
+//	public ModelAndView testGreeting(@RequestParam(value = "greeting", required=false)String greeting) {
+//		if(null == greeting) {
+//			greeting = "Hello There";
+//		}
+//		ModelAndView mav = new ModelAndView("page");
+//		mav.addObject("greeting", greeting);
+//		return mav;
+//	}
+
+//	@RequestMapping(value="/test/{greeting}")
+//	public ModelAndView testGreeting(@PathVariable("greeting")String greeting) {
+//		if(null == greeting) {
+//			greeting = "Hello There";
+//		}
+//		ModelAndView mav = new ModelAndView("page");
+//		mav.addObject("greeting", greeting);
+//		return mav;
+//	}
+}
